c353d687465ead1e8e739ff84da2c78ab6336a4f
Adi Adnan <adi.adnan15@yahoo.com>
2015-10-15 16:26:32 +0300

    Getting started.

---
+++ src/Printers/AbstractPrint.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Printers;
+
+/**
+ *
+ * @author ADI
+ */
+public class AbstractPrint {
+    
+    public static void printLine(String outputText){
+        
+        System.out.println(outputText);
+        
+    }
+    
+    
+}
---
+++ src/inteligentaartificiala_2/InteligentaArtificiala_2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inteligentaartificiala_2;
+
+import Printers.AbstractPrint;
+
+/**
+ *
+ * @author ADI
+ */
+public class InteligentaArtificiala_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
